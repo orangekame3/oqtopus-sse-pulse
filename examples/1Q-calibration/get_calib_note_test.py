0ac938afeca6fb76f78ec209f7c0988dff1d04ec
KazuhisaOgawa <92289392+KazuhisaOgawa@users.noreply.github.com>
2025-11-15 17:41:44 +0900

    Add test scripts for calibration note retrieval and library usage
    - Created `get_calib_note_test.py` to test the retrieval of calibration notes from the Experiment class, including JSON serialization of the calibration note.
    - Created `use_libs_test.py` to test the usage of the `hello` function from the `oqtopus_sse_pulse.libs.ogawa.my_func` module, with JSON output of the result.

---
+++ examples/1Q-calibration/get_calib_note_test.py
@@ -0,0 +1,39 @@
+import traceback
+import json
+import yaml
+
+from qubex.experiment import Experiment
+
+print("start program")
+try:
+    exp = Experiment(
+        chip_id="64Qv3",
+        muxes=[9],
+        params_dir="/sse/in/repo/ogawa/params",
+        calib_note_path="/sse/in/repo/ogawa/calib_note.json"
+    )
+    exp.connect()
+    res = exp.obtain_rabi_params(targets="Q36")
+    calib_note = exp.calib_note
+
+    # Convert CalibrationNote to dict for JSON serialization
+    calib_note_dict = calib_note._dict if calib_note else None
+    result: dict = {"mode": "", "data": {},"calib_note": calib_note_dict}
+    # for qubit, data in res.data.items():
+    #     result["data"][qubit] = {
+    #         "raw": {
+    #             "I": data.data.real.tolist(), # type: ignore
+    #             "Q": data.data.imag.tolist(), # type: ignore
+    #         },
+    #         "normalized": {
+    #             "I": data.normalized.real.tolist(), # type: ignore
+    #             "Q": data.normalized.imag.tolist(), # type: ignore
+    #         }
+    #     }
+    print("payload=" + json.dumps(result, ensure_ascii=False, separators=(",", ":")))
+
+except Exception as e:
+    print("Exception:", e)
+    traceback.print_exc()
+finally:
+    print("end program")
