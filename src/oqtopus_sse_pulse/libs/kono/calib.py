0c0d18b009ae54ac1e0e3caf749748b5dacbe409
SatoshiKONO0502 <kono.satoshi@sec.co.jp>
2025-12-18 08:46:31 +0900

    KONO: updated calib.py

--- src/oqtopus_sse_pulse/libs/kono/calib.py
+++ src/oqtopus_sse_pulse/libs/kono/calib.py
@@ -1,6 +1,44 @@
 import qubex as qx
 import json
 
+from qubex.experiment.mixin.characterization_mixin import CharacterizationMixin, DEFAULT_INTERVAL, DEFAULT_SHOTS, SAMPLING_PERIOD
+from typing import Collection, Literal
+import numpy as np
+from numpy.typing import ArrayLike, NDArray
+
+
+class MyCharacterization(CharacterizationMixin):
+    def calibrate_control_frequency(
+        self,
+        targets: Collection[str] | str | None = None,
+        *,
+        detuning_range: ArrayLike = np.linspace(-0.01, 0.01, 21),
+        time_range: ArrayLike = range(0, 101, 4),
+        frequencies: dict[str, float] | None = None,
+        amplitudes: dict[str, float] | None = None,
+        shots: int = DEFAULT_SHOTS,
+        interval: float = DEFAULT_INTERVAL,
+        plot: bool = True,
+    ) -> dict[str, float]:
+        result = self.chevron_pattern(
+            targets=targets,
+            detuning_range=detuning_range,
+            time_range=time_range,
+            frequencies=frequencies,
+            amplitudes=amplitudes,
+            shots=shots,
+            interval=interval,
+            plot=plot,
+            save_image=False,
+        )
+        resonant_frequencies = result["resonant_frequencies"]
+
+        print("\nResults\n-------")
+        print("ge frequency (GHz):")
+        for target, frequency in resonant_frequencies.items():
+            print(f"    {target}: {frequency:.6f}")
+        return resonant_frequencies
+
 
 def calibrate(ex: qx.Experiment):
     # calibrate
@@ -8,10 +46,15 @@ def calibrate(ex: qx.Experiment):
     ex.obtain_rabi_params(plot=False)
     control_frequencies = ex.calibrate_control_frequency(plot=False)
     ex.modified_frequencies(control_frequencies)
-    print(ex.system_manager._config_loader._props_dict)
-    # ex.calibrate_hpi_pulse(plot=False)
-    # t1 = ex.t1_experiment(plot=False)
-    # t1 = t1.data
+    # print(ex.system_manager._config_loader._props_dict)
+    ex.calibrate_hpi_pulse(plot=False)
+    t1 = ex.t1_experiment(plot=False)
+    t1 = t1.data
+    print(t1["Q37"].t1)
+    ex.modified_frequencies({"Q37": 8.0})
+    t1 = ex.t1_experiment(plot=False)
+    t1 = t1.data
+    print(t1["Q37"].t1)
     # t2 = ex.t2_experiment(plot=False)
     # t2 = t2.data
     # cls = ex.build_classifier(plot=False)
