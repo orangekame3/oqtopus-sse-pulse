943fa12035330fb9e2daa3ee1b56337e71c54f29
ryhiro <166011941+ryhiro@users.noreply.github.com>
2025-12-31 15:47:14 +0900

    Update custom_f.py

--- src/oqtopus_sse_pulse/libs/hirose/custom_f.py
+++ src/oqtopus_sse_pulse/libs/hirose/custom_f.py
@@ -6,6 +6,9 @@ from typing import Collection, Literal
 import numpy as np
 from numpy.typing import ArrayLike, NDArray
 
+from qubex.pulse import Pulse, PulseSchedule, FlatTop, Blank
+
+targets = ['Q36','Q37','Q38','Q39','Q40','Q43']
 
 class CustomCharacterizationMixin(CharacterizationMixin):
     # define the custom version of calibrate_control_frequency()
@@ -43,19 +46,7 @@ class CustomCharacterizationMixin(CharacterizationMixin):
 
 class CustomExperiment(CustomCharacterizationMixin, qx.Experiment):
     # inherit the custom calibrate_control_frequency function by extending CustomCharacterizationMixin
-#    pass
-    def calib_meas(
-            self,
-            counts: int = DEFAULT_SHOTS,
-            interval: int = DEFAULT_INTERVAL,
-            idle_time: int = 200,
-        ):
-    
-        for i in range(5):
-            print(f"hello {i}")
-        # output
-        result: dict = {"test": 1}
-        return result
+    pass
 
 
 def calibrate(ex: CustomExperiment):
@@ -118,3 +109,76 @@ def calibrate(ex: CustomExperiment):
     # output
     result: dict = {"calib_note": calib_note_dict, "props": props, "raw_data": raw_data}
     print("payload=" + json.dumps(result, ensure_ascii=False, separators=(",", ":")))
+
+
+class CustomExperiment2(CustomCharacterizationMixin, qx.Experiment):
+    # inherit the custom calibrate_control_frequency function by extending CustomCharacterizationMixin
+#    pass
+    def calib_meas(
+            self,
+            counts: int = DEFAULT_SHOTS,
+            interval: int = DEFAULT_INTERVAL,
+            idle_time: int = 200,
+            duration: int = 32,
+        ):
+    
+        for i in range(5):
+            print(f"hello {i}")
+
+
+        # calibration実行
+        calibrate(self)
+
+        # summarize results
+        calib_note = self.calib_note
+        # Convert CalibrationNote to dict for JSON serialization
+        calib_note_dict = calib_note._dict if calib_note else None
+
+        # 各qubitに対応するhpiパルスを作成
+        hpi_pulse = {}
+        pi_pulse = {}
+        for target in targets:
+            hpi_pulse[target] = FlatTop(
+                                        duration=duration,
+                                        amplitude=calib_note_dict['hpi_params'][target]['amplitude'],
+                                        tau=12,
+                                    )
+            pi_pulse[target] = FlatTop(
+                                        duration=duration,
+                                        amplitude=calib_note_dict['pi_params'][target]['amplitude'],
+                                        tau=12,
+                                    )
+
+        #sequence 定義
+        def sequence(time_idle: int) -> PulseSchedule:
+            with PulseSchedule(targets) as ps:
+                for target in targets:
+                    # ps.add(target, hpi_pulse[target])
+                    # ps.add(target, hpi_pulse[target])
+                    ps.add(target, pi_pulse[target])
+                    ps.add(target, Blank(time_idle))  # 指定した待ち時間だけ待機
+            return ps
+        
+        # measureメソッドで測定を実行
+        res = self.measure(
+            interval = interval,
+            sequence = sequence(idle_time), # 自作の波形シーケンスを指定
+            mode = "single", # 単発射影測定の場合は"single"を指定
+            shots = counts # ショット数
+        ) # MeasureResultクラスを出力する
+
+        # 結果を整形してJSON形式で出力0
+        result = {}
+        result["time_idle"] = idle_time,
+        result["counts_mes"] = counts,
+        result["time_list"] = (np.arange(len(res.data[target].raw)) * (interval + idle_time + duration)).tolist(),  # 読み出しのサンプリング間隔は8ns
+        for target in targets:
+            result[target] = {
+                "data_real": res.data[target].raw.real.tolist(),
+                "data_imag": res.data[target].raw.imag.tolist(),
+                "classed_data": cls[target].predict(res.data[target].raw).tolist(),
+            }
+
+        result: dict = {"test": 1}
+        return result
+
