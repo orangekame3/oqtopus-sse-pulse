be74d23283edcb18f870c0237239caebe1840265
SatoshiKONO0502 <kono.satoshi@sec.co.jp>
2026-01-07 22:34:50 +0900

    KONO: push my codes

--- review/kono/program/07_calibration_v2.py
+++ review/kono/program/07_calibration_v2.py
@@ -15,19 +15,16 @@ import datetime
 
 # 使用するqubit等の設定
 chip_id = '64Qv3'
-# muxes = [9, 10]
-qubits = ['Q36', 'Q38','Q39','Q40','Q42'] # 測定対象のqubitリスト
-# qubits = ['Q36', 'Q38', 'Q39', 'Q40']  # 測定対象のqubitリスト
-# exclude_qubits = ['Q37', 'Q41', 'Q42', 'Q43']  # 除外するqubitリスト
+qubits = ['Q36', 'Q38', 'Q40'] # 測定対象のqubitリスト
+# qubits = ['Q36', 'Q37', 'Q38', 'Q39', 'Q40', 'Q41', 'Q42', 'Q43'] # 測定対象のqubitリスト
 time_idle = 1000  # 各hpiパルス間の待ち時間(ns)
 counts_mes = 10_000  # 測定回数 (1 ms <=> 10 shots <=> 700 B ~ 1 KB)
 params_dir = "/sse/in/repo/kono/params"
 calib_note_path = "/sse/in/repo/kono/calib_note.json"
 duration = 32  # hpiパルスの全体の長さ(ns)
 
-try:
-    time_before_calibration = time.time()
 
+try:
     # CustomExperimentクラスのインスタンスを作成
     ex = CustomExperiment(
         chip_id=chip_id,
@@ -40,15 +37,8 @@ try:
     ex.connect()
 
     # calibration
-    calibrate(ex, calib_readout=True)   # Warning!: just measures readout frequencies, not runs calibration
-
-    # time_after_calibration = time.time()
-    # print(f"calibration time: {time_after_calibration - time_before_calibration} seconds")
-
-    # classifiers = ex.measurement._classifiers
-
-    # time_after_generate_classifiers = time.time()
-    # print(f"classifier generation time: {time_after_generate_classifiers - time_after_calibration} seconds")
+    calibrate(ex)   # Warning!: just measures readout frequencies, not runs calibration
+    # calibrate(ex, calib_readout=True)   # Warning!: just measures readout frequencies, not runs calibration
 
 
 # 例外処理
--- review/kono/program/07_repeat_single_shot_v3.py
+++ review/kono/program/07_repeat_single_shot_v3.py
@@ -7,25 +7,28 @@ from qubex.pulse import PulseSchedule, FlatTop, Blank
 import numpy as np
 import json
 
-from oqtopus_sse_pulse.libs.kono.calib import calibrate, CustomExperiment
+from oqtopus_sse_pulse.libs.kono.calib import calibrate, restore_classifiers_from_base64, CustomExperiment
+# from oqtopus_sse_pulse.libs.kono.calib import calibrate, CustomExperiment
 
 import time
-import datetime
+# import datetime
+
+import base64
 
 
 # 使用するqubit等の設定
 chip_id = '64Qv3'
 # muxes = [9, 10]
-qubits = ['Q36', 'Q38','Q39','Q40','Q42'] # 測定対象のqubitリスト
-# qubits = ['Q36', 'Q38', 'Q39', 'Q40']  # 測定対象のqubitリスト
-# exclude_qubits = ['Q37', 'Q41', 'Q42', 'Q43']  # 除外するqubitリスト
+qubits = ['Q36', 'Q38', 'Q40'] # 測定対象のqubitリスト
+# qubits = ['Q36', 'Q38', 'Q39', 'Q40', 'Q42'] # 測定対象のqubitリスト
 time_idle = 1000  # 各hpiパルス間の待ち時間(ns)
-counts_mes = 10_000  # 測定回数 (1 ms <=> 10 shots <=> 700 B ~ 1 KB)
+counts_mes = 100_000  # 測定回数
 params_dir = "/sse/in/repo/kono/params"
 calib_note_path = "/sse/in/repo/kono/calib_note.json"
 duration = 32  # hpiパルスの全体の長さ(ns)
 interval = 100 * 1000  # 測定から初期化までの待機時間(ns単位)
-max_execution_time = 1200 * 0.05  # 最大実行時間（秒）
+max_execution_time = 1200 * 0.5  # 最大実行時間（秒）
+recalib_cls = True  # 分類器を動的にrecalibrateするかどうか
 
 try:
     time_before_calibration = time.time_ns()
@@ -41,14 +44,28 @@ try:
     )
     ex.connect()
 
-    # calibration
+    # # calibration
     # calibrate(ex)
-    ex.build_classifier(plot=False)
 
-    time_after_calibration = time.time_ns()
-    print(f"Calibration time: {time_after_calibration - time_before_calibration} seconds")
+    # prepare classifiers
+    if recalib_cls:
+        cls = ex.build_classifier(plot=False)
+        # ex.build_classifier(plot=False)
 
-    classifiers = ex.measurement._classifiers
+        time_after_calibration = time.time_ns()
+        print(f"Calibration time: {time_after_calibration - time_before_calibration} seconds")
+
+        classifiers = ex.classifiers
+
+        # output
+        props = {
+            "readout_fidelities": cls["readout_fidelities"],
+            "average_readout_fidelity": cls["average_readout_fidelity"],
+        }
+        result: dict = {"props": props}
+        print("payload=" + json.dumps(result, ensure_ascii=False, separators=(",", ":")))
+    else:
+        classifiers = restore_classifiers_from_base64()
 
     # classifiers is a dict in the form of: {"Q36": classifier_of_Q36, "Q37": classifier_of_Q37, ...}
     # call classifiers["Q36"].predict(data) to predict the state of qubit Q36 for given data.
@@ -105,7 +122,16 @@ try:
             "states": {}
         }
         for target in targets:
-            result["states"][target] = classifiers[target].predict(res.data[target].raw.reshape(-1)).tolist()
+            # result["states"][target] = classifiers[target].predict(res.data[target].raw.reshape(-1)).tolist()
+            arr = classifiers[target].predict(res.data[target].raw.reshape(-1))
+            packed = np.packbits(arr)
+            # バイト列をbase64でエンコード
+            packed_b64 = base64.b64encode(packed.tobytes()).decode('ascii')
+            result["states"][target] = packed_b64
+            result["num_shots"] = len(arr)
+
+            # # デバッグ
+            # print(f"Qubit {target}: States: {arr.tolist()}")
 
         # 結果の出力
         print("payload=" + json.dumps(result, ensure_ascii=False, separators=(",", ":")))
