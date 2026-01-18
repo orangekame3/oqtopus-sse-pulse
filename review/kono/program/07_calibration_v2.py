e20ed85dca2234359e2acec6a2e762c6d436ccff
SatoshiKONO0502 <kono.satoshi@sec.co.jp>
2026-01-18 17:02:49 +0900

    KONO: updated my codes in rev folder

--- review/kono/program/07_calibration_v2.py
+++ review/kono/program/07_calibration_v2.py
@@ -15,10 +15,22 @@ from oqtopus_sse_pulse.libs.kono.calib import calibrate, CustomExperiment
 
 # 使用するqubit等の設定
 chip_id = '64Qv3'
-qubits = ['Q36', 'Q37', 'Q38', 'Q42', 'Q43'] # 測定対象のqubitリスト
-# qubits = ['Q36', 'Q37', 'Q38', 'Q39', 'Q40', 'Q41', 'Q42', 'Q43'] # 測定対象のqubitリスト
-# qubits = ['Q36', 'Q37', 'Q38', 'Q42', 'Q43', 'Q52', 'Q54', 'Q55'] # 測定対象のqubitリスト
+qubits_mux09 = ['Q36', 'Q37', 'Q38', 'Q39'] # MUX09に接続されている測定対象のqubitリスト
+qubits_mux10 = ['Q41', 'Q42'] # MUX10に接続されている測定対象のqubitリスト
+# qubits_mux10 = ['Q40', 'Q41', 'Q42', 'Q43'] # MUX10に接続されている測定対象のqubitリスト
+qubits_mux11 = ['Q44', 'Q47'] # MUX11に接続されている測定対象のqubitリスト
+# qubits_mux11 = ['Q44', 'Q45', 'Q46', 'Q47'] # MUX11に接続されている測定対象のqubitリスト
+qubits_mux12 = ['Q48', 'Q49', 'Q50', 'Q51'] # MUX12に接続されている測定対象のqubitリスト
+qubits_mux13 = ['Q52', 'Q54', 'Q55'] # MUX13に接続されている測定対象のqubitリスト
+# qubits_mux13 = ['Q52', 'Q53', 'Q54', 'Q55'] # MUX13に接続されている測定対象のqubitリスト
+qubits_mux14 = ['Q56', 'Q57', 'Q58', 'Q59'] # MUX14に接続されている測定対象のqubitリスト
+qubits_mux15 = ['Q60', 'Q62', 'Q63'] # MUX15に接続されている測定対象のqubitリスト
+# qubits_mux15 = ['Q60', 'Q61', 'Q62', 'Q63'] # MUX15に接続されている測定対象のqubitリスト
+qubits = qubits_mux09 + qubits_mux10
+# qubits = qubits_mux13 + qubits_mux14
+# qubits = qubits_mux11 + qubits_mux15
 # qubits = ['Q36', 'Q37', 'Q38', 'Q39', 'Q40', 'Q41', 'Q42', 'Q43', 'Q48', 'Q49', 'Q50', 'Q51', 'Q52', 'Q53', 'Q54', 'Q55'] # 測定対象のqubitリスト
+# qubits = ['Q36', 'Q37', 'Q38', 'Q42', 'Q43', 'Q52', 'Q54', 'Q55'] # 測定対象のqubitリスト
 params_dir = "/sse/in/repo/kono/params"
 calib_note_path = "/sse/in/repo/kono/calib_note.json"
 
